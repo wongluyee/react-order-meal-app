e283ab190ec56c334a0821970f054f62fa0a65f6
Luyee Wong <wongluyee1015@hotmail.com>
2023-05-10 13:54:09 +0900

    add cart button

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # react-order-meal-app
 An app for ordering meal
 
-Learning React by following a tutorial and built this app.
+Learning React by following a tutorial and building this app.
 Still in progress...
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCart from './HeaderCart';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCart />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="meals header" />
