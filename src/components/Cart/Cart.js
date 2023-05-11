380ae9eab73348e026ec87edf90ec5f3dd781228
Luyee Wong <wongluyee1015@hotmail.com>
2023-05-11 20:32:56 +0900

    manage the modal state

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,14 +8,14 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       <ul className={classes['cart-items']}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>60.00</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
